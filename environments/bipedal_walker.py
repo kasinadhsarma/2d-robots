1f85b545b9b3d675d04577fe27fda8f76a34d5db
kasinadhsarma <kasinadhsarma@gmail.com>
2024-07-11 22:04:28 +0000

    Fix E999 syntax error by correcting multi-line f-string

--- environments/bipedal_walker.py
+++ environments/bipedal_walker.py
@@ -914,8 +914,8 @@ class BipedalWalkerV2(gym.Env, EzPickle):
         if self.render_mode is None:
             assert self.spec is not None
             gym.logger.warn(
-                "You are calling render method without specifying any render mo
-                "You can specify the render_mode at initialization, "
+                f"You are calling render method without specifying any render "
+                f"mode. You can specify the render_mode at initialization, "
                 f'gym.make("{self.spec.id}", render_mode="rgb_array")'
             )
             return
